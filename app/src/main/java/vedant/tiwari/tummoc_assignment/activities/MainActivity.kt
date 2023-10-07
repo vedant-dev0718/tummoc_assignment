f13ac1b56af910e1cccf07c34ffabc2da45cb993
vedant tiwari <tiwarivedant.1690@gmail.com>
2023-10-08 01:20:40 +0530

    Initial commit

---
+++ app/src/main/java/vedant/tiwari/tummoc_assignment/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package vedant.tiwari.tummoc_assignment.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vedant.tiwari.tummoc_assignment.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
